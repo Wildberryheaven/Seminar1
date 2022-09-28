3d1397e0d7313fc6aedf0ea415c279b1a0cb3e25
Denis Mitrofanov <wildberryheaven@gmail.com>
2022-09-28 17:42:37 +0300

    Добавлена Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numbercheck = -number;
+while (numbercheck <= number) {
+    Console.Write(numbercheck + " ");
+    numbercheck++;
+}
