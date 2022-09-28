d630f6461daed570a9628aade3f35e6f8532aa37
Denis Mitrofanov <wildberryheaven@gmail.com>
2022-09-28 18:13:08 +0300

    Добавлена Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) {
+    max = b;
+}
+if (max < c) {
+    max = c;
+}
+Console.WriteLine("Max = " + max);
