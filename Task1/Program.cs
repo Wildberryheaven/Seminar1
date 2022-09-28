a94b7f6a96de92d8f7f7f98f1c7ed0e45628d36f
Denis Mitrofanov <wildberryheaven@gmail.com>
2022-09-28 16:14:13 +0300

    all

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,13 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 == number2 / number1) {
-    Console.WriteLine("Первое число является квадратом второго");
-} else {
-    Console.WriteLine("Первое число не является квадратом второго");
+if (number1 == number2 / number1)
+{
+    Console.WriteLine($"Первое число ({number1}) является квадратом второго ({number2})");
+}
+else
+{
+    Console.WriteLine($"Первое число ({number1}) не является квадратом второго ({number2})");
 }
