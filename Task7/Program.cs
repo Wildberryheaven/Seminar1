57e7045f47150ae5c78d922523420a5a7e4fb34e
Denis Mitrofanov <wildberryheaven@gmail.com>
2022-09-29 20:32:16 +0300

    Добавлена задача Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number % 10;
+Console.WriteLine($"Последняя цифра числа {number} равна: {number1}");
