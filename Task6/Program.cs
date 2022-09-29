4044be05357f77bc759517e2c5a98a31f7e2b600
Denis Mitrofanov <wildberryheaven@gmail.com>
2022-09-29 18:59:41 +0300

    Добавлена задача Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) {
+    Console.WriteLine($"Число {number} чётное");
+} else {
+    Console.WriteLine($"Число {number} нечётное");
+}
